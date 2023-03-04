ab1bd664be8425053be5132df8dff2a7682d4cb7
Ella Soccoli <soccoli2015@gmail.com>
2023-03-03 20:23:07 -0500

    Updated resume file

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = flask.Flask(__name__)
 @app.route('/')
 
 def resume():
-    return flask.send_file("Ella_Soccoli_Resume_Feb_2023.pdf")
+    return flask.send_file("Ella_Soccoli_Resume_Mar_2023.pdf")
 
 app.run()
